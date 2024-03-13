1f2d42bbced14f3b6fdc349b6774764a1c4f93dd
Ian Clarkson <clarksonian@gmail.com>
2024-03-12 22:23:27 -0700

    Want to fix up spaces in all compared names

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-https://apps.timwhitlock.info/emoji/tables/iso3166
+# Flombo - Flag Combos!
+
+A friend of mine came up with a game whereby he'd take two countries and mash their flags together. The player's task was to tease apart which two country's flags had been combined, and *then* figure out the third (actual!) country name they represented by taking letters from the start and end of the first two countries.
+
+I tried to explain that and I think it's probably more confusing afterwards.
+
+Here's an example:
+
+Can you tell which country flags have been combined?
+
+That's right! It's Canada and Greece!
+
+And if we take the first three letters of GREece and the last four letters of caNADA and mash them together, what do you get?
+
+GRENADA -- the name of another country!
+
+So we can pretend like this is the flag of Grenada, which it's not. But that's the game.
+
+I decided to whip up a quick script to try to determine all of the possibilities. In my friend's original version, the flags were artfully combined. In my version, I just wanted to check out which ones were valid. And that's what you see here!
+
+# The Results
+
+Here are the results in *flag form* for you to try to guess. Don't scroll too quickly or you'll see the text translation and spoil all of the answers!
+
+
+# Credits
+
+- Thanks to https://apps.timwhitlock.info/emoji/tables/iso3166 for the list of countries their emoji flags
--- index.ts
+++ index.ts
@@ -1,1 +1,64 @@
-console.log("Hello via Bun!");
+import { readFileSync } from "fs";
+
+const HIDE = false;
+
+const data = readFileSync("flags.txt", "utf8")
+  .split("\n")
+  .filter((line) => line.length > 0)
+  .map((line) => line.split("\t"));
+
+for (let t = 0; t < data.length; t++) {
+  const target = data[t][3];
+
+  // Score all countries as "starts" according to how many letters in from the start it shares with the target
+  const startScores = data.map((country, idx) => {
+    if (idx === t) return 0;
+    let score = 0;
+    for (let i = 0; i < target.length; i++) {
+      if (country[3][i] === target[i]) {
+        score++;
+      } else {
+        break;
+      }
+    }
+    // Whole word is contained ("Samoa" in "American Samoa", which is not interesting)
+    if (score === target.length) {
+      score = 0;
+    }
+    return score;
+  });
+
+  // Do the same but as "ends" (from the end)
+  const endScores = data.map((country, idx) => {
+    if (idx === t) return 0;
+    let score = 0;
+    for (let i = 0; i < target.length; i++) {
+      if (
+        country[3][country[3].length - 1 - i] === target[target.length - 1 - i]
+      ) {
+        score++;
+      } else {
+        break;
+      }
+    }
+    // Whole word is contained ("Samoa" in "American Samoa", which is not interesting)
+    if (score === target.length) {
+      score = 0;
+    }
+    return score;
+  });
+
+  // See if we can find a start score and end score where indexes aren't the same
+  // where the total score is >= length of the target
+  for (let i = 0; i < startScores.length; ++i) {
+    for (let j = 0; j < endScores.length; ++j) {
+      if (i !== j && startScores[i] + endScores[j] >= target.length) {
+        const idx = HIDE ? 1 : 3;
+        const start = data[i][idx];
+        const end = data[j][idx];
+        const result = data[t][idx];
+        console.log(`${start}  +  ${end}  =  ${result}`);
+      }
+    }
+  }
+}
