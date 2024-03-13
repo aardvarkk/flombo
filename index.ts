39cb1ffc255c8de7c436968b5324195f35fc684f
Ian Clarkson <clarksonian@gmail.com>
2024-03-12 22:28:02 -0700

    Factor in commas and spaces and stuff -- remove them and compare those

--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
 import { readFileSync } from "fs";
 
 const HIDE = false;
+const FLAG_INDEX = 1;
+const NAME_INDEX = 3;
+const NORM_INDEX = 4;
 
 const data = readFileSync("flags.txt", "utf8")
   .split("\n")
   .filter((line) => line.length > 0)
   .map((line) => line.split("\t"));
 
+data.forEach((row) => {
+  row.push(row[NAME_INDEX].toLowerCase().replace(/[^a-z]/g, ""));
+});
+
 for (let t = 0; t < data.length; t++) {
-  const target = data[t][3];
+  const target = data[t][NORM_INDEX];
 
   // Score all countries as "starts" according to how many letters in from the start it shares with the target
   const startScores = data.map((country, idx) => {
     if (idx === t) return 0;
     let score = 0;
     for (let i = 0; i < target.length; i++) {
-      if (country[3][i] === target[i]) {
+      if (country[NORM_INDEX][i] === target[i]) {
         score++;
       } else {
         break;
@@ -34,7 +41,8 @@ for (let t = 0; t < data.length; t++) {
     let score = 0;
     for (let i = 0; i < target.length; i++) {
       if (
-        country[3][country[3].length - 1 - i] === target[target.length - 1 - i]
+        country[NORM_INDEX][country[NORM_INDEX].length - 1 - i] ===
+        target[target.length - 1 - i]
       ) {
         score++;
       } else {
@@ -53,7 +61,7 @@ for (let t = 0; t < data.length; t++) {
   for (let i = 0; i < startScores.length; ++i) {
     for (let j = 0; j < endScores.length; ++j) {
       if (i !== j && startScores[i] + endScores[j] >= target.length) {
-        const idx = HIDE ? 1 : 3;
+        const idx = HIDE ? FLAG_INDEX : NAME_INDEX;
         const start = data[i][idx];
         const end = data[j][idx];
         const result = data[t][idx];
